d44759ff66682cc5356d97eff0df6eccd02fc239
newbee2012 <newbee2012@126.com>
2024-11-29 17:56:08 +0800

    Modify GPIO pin definitions to support ESP32-S3.

--- src/modcamera.h
+++ src/modcamera.h
@@ -22,12 +22,12 @@ enum { OV2640, OV7725};
 #define TAG "camera"
 
 //WROVER-KIT PIN Map
+/*
 #define CAM_PIN_PWDN    32 //power down is not used
 #define CAM_PIN_RESET   -1 //software reset will be performed
 #define CAM_PIN_XCLK     0
 #define CAM_PIN_SIOD    26 // SDA
 #define CAM_PIN_SIOC    27 // SCL
-
 #define CAM_PIN_D7      35
 #define CAM_PIN_D6      34
 #define CAM_PIN_D5      39
@@ -39,6 +39,29 @@ enum { OV2640, OV7725};
 #define CAM_PIN_VSYNC   25
 #define CAM_PIN_HREF    23
 #define CAM_PIN_PCLK    22
+*/
+///////////////////////////////////
+#define CAM_PIN_PWDN    -1
+#define CAM_PIN_RESET   -1
+#define CAM_PIN_XCLK     15
+#define CAM_PIN_SIOD    4
+#define CAM_PIN_SIOC    5
+#define CAM_PIN_D0       11
+#define CAM_PIN_D1      9
+#define CAM_PIN_D2      8
+#define CAM_PIN_D3      10
+#define CAM_PIN_D4      12
+#define CAM_PIN_D5      18
+#define CAM_PIN_D6      17
+#define CAM_PIN_D7      16
+#define CAM_PIN_VSYNC   6
+#define CAM_PIN_HREF    7
+#define CAM_PIN_PCLK    13
+///////////////////////////////////
+
+
+
+
 #define XCLK_FREQ_10MHz    10000000
 #define XCLK_FREQ_20MHz    20000000
 
